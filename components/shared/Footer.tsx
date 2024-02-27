675954941b59d26c3d6e2903d4d0f287ffa0f535
aayush <aausgh@gmail.com>
2024-02-27 19:28:43 +0545

    feat: added footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
       return (
-            <div>Footer</div>
+            <footer className='border-t'>
+                  <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+                        <Link href="/" >
+                              <Image
+                                    src="/assets/images/logoo.svg"
+                                    width={150}
+                                    height={68}
+                                    alt="logo"
+                              />
+                        </Link>
+
+                        <p>2023 Eventopia. All rights reserved</p>
+                  </div>
+
+            </footer>
       )
 }
 
