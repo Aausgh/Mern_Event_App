7a310bddefec54244b7e09d8f6ed4486552a7738
aayush <aausgh@gmail.com>
2024-02-29 17:26:02 +0545

    event details page

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,17 +1,26 @@
-"use server"
+"use server";
 
-import { connectToDatabase } from '../database';
-import Event from '../database/models/event.model';
-import User from '../database/models/user.model';
-import { handleError } from '../utils';
-import { CreateEventParams } from './../../types/index';
+import { connectToDatabase } from "../database";
+import Category from "../database/models/category.model";
+import Event from "../database/models/event.model";
+import User from "../database/models/user.model";
+import { handleError } from "../utils";
+import { CreateEventParams } from "./../../types/index";
 
+const populateEvent = async (query: any) => {
+      return query
+            .populate({
+                  path: 'organizer', model: User, select: '_id firstName lastName'
+            })
+            .populate({
+                  path: 'category', model: Category, select: '_id name'
+            })
+}
 
-
-export const createEvent = async ({ event, userId, path }: CreateEventParams) =>{
+export const createEvent = async ({ event, userId, path, }: CreateEventParams) => {
       try {
             await connectToDatabase();
-            
+
             const organizer = await User.findById(userId);
 
             if (!organizer) {
@@ -21,12 +30,28 @@ export const createEvent = async ({ event, userId, path }: CreateEventParams) =>
             const newEvent = await Event.create({
                   ...event,
                   category: event.categoryId,
-                  organizer: userId
+                  organizer: userId,
             });
-            
+
             return JSON.parse(JSON.stringify(newEvent));
+      } catch (error) {
+            handleError(error);
+      }
+};
+
+export const getEventById = async (eventId: string) => {
+      try {
+            await connectToDatabase();
+
+            const event = await populateEvent(Event.findById(eventId));
+
+            if (!event) {
+                  throw new Error("Event not found");
+            }
+
+            return JSON.parse(JSON.stringify(event));
 
       } catch (error) {
             handleError(error);
       }
-}
+};
